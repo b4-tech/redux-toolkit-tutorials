d63549858e38e21ac6ee0299171c969b2ea99f89
b4-tech <hcetanutrof@gmail.com>
2023-01-03 16:21:28 +0200

    lesson-09. bind action creators

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,5 +1,6 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
@@ -40,7 +41,8 @@ const unsubscribe = store.subscribe(() =>
   console.log("Updated state", store.getState())
 );
 
-store.dispatch(orderCake());
-store.dispatch(restockCake(5));
+const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch);
+actions.orderCake();
+actions.restockCake(5);
 
 unsubscribe();
