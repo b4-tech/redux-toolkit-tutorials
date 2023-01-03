29f55404d412726e9da5f633bc2a12ed0521c13b
b4-tech <hcetanutrof@gmail.com>
2023-01-03 18:22:54 +0200

    lesson-15. async actions

---
+++ redux-demo/asyncActions.js
@@ -0,0 +1,59 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const initialState = {
+  loading: false,
+  users: [],
+  error: null,
+};
+
+const FETCH_USERS_REQUESTED = "FETCH_USERS_REQUESTED";
+const FETCH_USERS_SUCCEEDED = "FETCH_USERS_SUCCEEDED";
+const FETCH_USERS_FAILED = "FETCH_USERS_FAILED";
+
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUESTED,
+  };
+};
+
+const fetchUsersSucceeded = () => {
+  return {
+    type: FETCH_USERS_SUCCEEDED,
+    payload: users,
+  };
+};
+
+const fetchUsersFailure = (error) => {
+  return {
+    type: FETCH_USERS_REQUESTED,
+    payload: error,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUESTED:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_USERS_SUCCEEDED:
+      return {
+        ...state,
+        loading: false,
+        users: action.payload,
+      };
+    case FETCH_USERS_SUCCEEDED:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(redux);
