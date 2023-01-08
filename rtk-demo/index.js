b96da87f15cbc18fa6581e36deac05c60bc26afd
b4-tech <hcetanutrof@gmail.com>
2023-01-08 22:20:15 +0200

    lesson-24. async thunks

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -2,9 +2,12 @@ const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const iceCreamActions =
   require("./features/iceCream/iceCreamSlice").iceCreamActions;
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
 
 store.dispatch(iceCreamActions.ordered());
 store.dispatch(iceCreamActions.restocked(5));
+
+store.dispatch(fetchUsers())
