ada58d002f299db46a8dcf7563f1641a28e1249c
b4-tech <hcetanutrof@gmail.com>
2023-01-12 11:05:25 +0200

    lesson-26. provider

--- react-rtk-demo/src/app/store.js
+++ react-rtk-demo/src/app/store.js
@@ -1,10 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { createLogger } from "redux-logger";
 import cakeReducer from "../features/cake/cakeSlice";
 import iceCreamReducer from "../features/iceCream/iceCreamSlice";
 import userReducer from "../features/user/userSlice";
-
-const logger = createLogger();
 
 const store = configureStore({
   reducer: {
@@ -12,7 +9,6 @@ const store = configureStore({
     iceCream: iceCreamReducer,
     user: userReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
