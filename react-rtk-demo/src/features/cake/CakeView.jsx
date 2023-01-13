e4f58c9dc049c15d921fa4b53472b24fa2ddd24a
b4-tech <hcetanutrof@gmail.com>
2023-01-13 14:23:59 +0200

    lesson-27. useSelector

--- react-rtk-demo/src/features/cake/CakeView.jsx
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import { useSelector } from "react-redux";
+import { ordered as CakeOrdered } from "./cakeSlice";
 
 const CakeView = () => {
+  const numberOfCakes = useSelector((state) => state.cake.numberOfCakes);
   return (
-	<div>
-		<h2>Number of cakes - </h2>
-		<button>Order cake</button>
-		<button>Restock cakes</button>
-	</div>
-  )
-}
+    <div>
+      <h2>Number of cakes - {numberOfCakes}</h2>
+      <button>Order cake</button>
+      <button>Restock cakes</button>
+    </div>
+  );
+};
 
-export default CakeView
+export default CakeView;
--- react-rtk-demo/src/features/iceCream/IceCreamView.jsx
+++ react-rtk-demo/src/features/iceCream/IceCreamView.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const IceCreamView = () => {
+  const numberOfIceCreams = useSelector(
+    (state) => state.iceCream.numberOfIceCreams
+  );
   return (
     <div>
-      <h2>Number of ice creams - </h2>
+      <h2>Number of ice creams - {numberOfIceCreams}</h2>
       <button>Order ice cream</button>
       <button>Restock ice creams</button>
     </div>
